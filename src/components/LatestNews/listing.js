de5ce44836338d81c331f6262defee27c244fd21
Saad <isaadansari@gmail.com>
2019-07-29 15:34:41 +0500

    latest news

---
+++ src/components/LatestNews/listing.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import { Text, RichText, DateField, Image , withSitecoreContext} from '@sitecore-jss/sitecore-jss-react';
+
+// const LatestNews = ({ sitecoreContext: { route: { fields } } }) => (
+//   <div>
+//     <Text field={fields.pageTitle} tag="h3" />
+//     <Text field={fields.newsHeading} tag="h4" />
+//     <RichText field={fields.newsDescription} />
+//     <Text field={fields.newsCategory} tag="h5" />
+//     <DateField field={fields.newsDate} render={date => date.toLocaleDateString()} tag="div" />
+//     <Image field={fields.newsImage}></Image>
+    
+//   </div>
+// );
+// export default withSitecoreContext()(LatestNews);
