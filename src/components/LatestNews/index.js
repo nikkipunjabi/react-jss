86db6201bcd9fedac2479aa522bf1dc0f0e833ec
Nikki Punjabi <nikki.punjabi@gmail.com>
2019-07-30 11:36:15 +0400

    Fixed News Listing Issue

--- src/components/LatestNews/index.js
+++ src/components/LatestNews/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Text, RichText, DateField, Image , withSitecoreContext} from '@sitecore-jss/sitecore-jss-react';
+import { Text, RichText, DateField, Image } from '@sitecore-jss/sitecore-jss-react';
 
-const LatestNews = ({ sitecoreContext: { route: { fields } } }) => (
-  <div>
-    <Text field={fields.pageTitle} tag="h3" />
-    <Text field={fields.newsHeading} tag="h4" />
-    <RichText field={fields.newsDescription} />
-    <Text field={fields.newsCategory} tag="h5" />
-    <DateField field={fields.newsDate} render={date => date.toLocaleDateString()} tag="div" />
-    <Image field={fields.newsImage}></Image>
-    
-  </div>
-);
-export default withSitecoreContext()(LatestNews);
+const LatestNews: React.FunctionComponent<Props> = (props) => {
+
+  const { latestNewsListing } = props.fields;
+
+  return (
+    <div>
+      <ul>
+        {latestNewsListing &&
+          latestNewsListing.map((listItem, index) => (
+            <li key={index}>
+              <Text field={listItem.fields.pageTitle} tag="h3" />
+              <Text field={listItem.fields.newsHeading} tag="h4" />
+              <RichText field={listItem.fields.newsDescription} />
+              <Text field={listItem.fields.newsCategory} tag="h5" />
+              <DateField field={listItem.fields.newsDate} render={date => date.toLocaleDateString()} tag="div" />
+              <Image field={listItem.fields.newsImage}></Image>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default LatestNews;
